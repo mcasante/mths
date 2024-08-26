9374dd3c7d19c1684b0067f2584725ae72df0285
Matheus Casante <matheus.casante@gmail.com>
2024-08-26 01:37:22 -0300

    feat: toggle dark mode; app layout and index

---
+++ src/components/ModeToggle.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { useColorMode } from "@vueuse/core";
+import { Sun, Moon, SunMoon } from "lucide-vue-next";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+const mode = useColorMode();
+</script>
+
+<template>
+  <DropdownMenu>
+    <DropdownMenuTrigger as-child>
+      <Button variant="outline">
+        <Sun
+          class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+        />
+        <Moon
+          class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+        />
+
+        <span class="sr-only">Toggle theme</span>
+      </Button>
+    </DropdownMenuTrigger>
+    <DropdownMenuContent align="end">
+      <DropdownMenuItem @click="mode = 'light'">
+        <Sun class="mr-2 h-4 w-4" />
+        Light
+      </DropdownMenuItem>
+      <DropdownMenuItem @click="mode = 'dark'">
+        <Moon class="mr-2 h-4 w-4" />
+        Dark
+      </DropdownMenuItem>
+      <DropdownMenuItem @click="mode = 'auto'">
+        <SunMoon class="mr-2 h-4 w-4" />
+        System
+      </DropdownMenuItem>
+    </DropdownMenuContent>
+  </DropdownMenu>
+</template>
